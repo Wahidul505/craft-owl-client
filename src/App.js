06bfc9985a1ae04117dc5a63a952644d25924609
Wahidul30 <wahidul505@gmail.com>
2022-05-22 01:25:08 +0600

    testing the repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./Pages/Home/Landing";
 function App() {
   return (
     <div data-theme="light">
+      <button className="btn btn-primary">Toggle</button>
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
