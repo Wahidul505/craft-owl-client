6b6de25bc18cfa5dc257c9606da5360287cb90c3
Wahidul30 <wahidul505@gmail.com>
2022-05-23 14:21:44 +0600

    implemented functionalities to adjust order quantity and showed predictable errors if quantity didn't match the requirements

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import LoadingSpinner from '../Shared/LoadingSpinner';
 import ToolCard from './ToolCard';
 
 const Tools = () => {
     const { data: tools, isLoading } = useQuery('display-tools', () => fetch('http://localhost:5000/tool')
         .then(res => res.json()));
     if (isLoading) {
-        return <h1>Loading...</h1>
+        return <LoadingSpinner />
     }
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BsPlusLg } from 'react-icons/bs';
+import { FaMinus } from 'react-icons/fa';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { useQuery } from 'react-query';
@@ -7,11 +9,31 @@ import auth from '../../firebase.init';
 import LoadingSpinner from '../Shared/LoadingSpinner';
 
 const Purchase = () => {
+    const [quantity, setQuantity] = useState(0);
+    const [displayError, setDisplayError] = useState('');
+    const [skipSteps, setSkipSteps] = useState(1);
     const { id } = useParams();
     const [user] = useAuthState(auth);
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const { data: tool, isLoading } = useQuery('purchasingTool', () => fetch(`http://localhost:5000/tool/${id}`)
         .then(res => res.json()));
+
+    useEffect(() => {
+        setQuantity(parseInt(tool?.minimumOrderQuantity));
+    }, [tool]);
+
+    useEffect(() => {
+        if (quantity < tool?.minimumOrderQuantity) {
+            setDisplayError('You have to Order more than Minimum Order Quantity');
+        }
+        else if (quantity > tool?.availableQuantity) {
+            setDisplayError('You have to Order below Available Stock');
+        }
+        else {
+            setDisplayError('');
+        }
+    }, [quantity, tool]);
+
     if (isLoading) {
         return <LoadingSpinner />
     };
@@ -21,18 +43,62 @@ const Purchase = () => {
         const person = user?.displayName;
         const email = user?.email;
         console.log(person, email, data);
+    };
+
+    const handleSkipQuantitySteps = e => {
+        setSkipSteps(parseInt(e.target.value));
     }
+
+    const handleIncreaseQuantity = () => {
+        setQuantity((previousQuantity) => previousQuantity + skipSteps);
+    };
+
+    const handleDecreaseQuantity = () => {
+        setQuantity((previousQuantity) => previousQuantity - skipSteps);
+    };
+
+
+
     return (
         <div className='mt-24 grid grid-cols-1 lg:grid-cols-3'>
             <div class="card md:card-side bg-base-100 shadow-xl lg:col-span-2">
                 <figure><img className='h-48 rounded-lg md:rounded-l-lg md:rounded-r-none md:h-full w-60' src={image} alt="Album" /></figure>
                 <div class="card-body">
-                    <h2 class="card-title text-2xl">{name}</h2>
+                    <h2 class="card-title text-3xl">{name}</h2>
                     <p className='text-gray-600' title={description}>{description.length > 60 ? description.slice(0, 60) + '...' : description}</p>
                     <p className='text-2xl'>Tk <span className='text-secondary font-semibold'>{price}</span></p>
-                    <div class="card-actions justify-start">
-                        <button class="btn btn-primary">Buy Now</button>
+                    <p>Minimum Order Quantity: <span className="text-secondary font-semibold">{minimumOrderQuantity}</span></p>
+                    <p>Available in Stock: <span className="text-secondary font-semibold">{availableQuantity}</span></p>
+                    {/* Quantity Handler  */}
+                    <div>
+                        <span className='mr-4'>Quantity:</span>
+                        <button
+                            onClick={handleDecreaseQuantity}
+                            disabled={quantity <= minimumOrderQuantity}
+                            className='btn btn-sm btn-secondary mr-2 font-bold'><FaMinus /></button>
+                        <input
+                            type="number" name="quantity"
+                            className='border border-primary rounded w-24 p-1'
+                            value={quantity || ''}
+                            disabled
+                        />
+                        <button
+                            onClick={handleIncreaseQuantity}
+                            disabled={quantity >= availableQuantity}
+                            className='btn btn-sm btn-secondary mx-2 font-bold'><BsPlusLg /></button>
+                        <select
+                            onChange={handleSkipQuantitySteps}
+                        >
+                            <option disabled selected>Skip</option>
+                            <option value='1'>1</option>
+                            <option value='50'>50</option>
+                            <option value='100'>100</option>
+                            <option value='200'>200</option>
+                        </select>
                     </div>
+                    <p className='text-error h-10'>
+                        {displayError ? displayError : ''}
+                    </p>
                 </div>
             </div>
             {/* form starts from here  */}
@@ -84,7 +150,7 @@ const Purchase = () => {
                     </div>
                     {/* submit button  */}
                     <div class="form-control mt-6">
-                        <button type='submit' class="btn btn-primary">Buy Now</button>
+                        <button type='submit' class="btn btn-primary" disabled={displayError}>Buy Now</button>
                     </div>
                 </form>
             </div>
