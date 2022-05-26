5e1ce37dfa7d29402c2b44a86ac35e6043750936
Wahidul30 <wahidul505@gmail.com>
2022-05-26 13:00:09 +0600

    implemented order updation by shipping an order and order deletion

---
+++ src/Pages/Dashboard/ManageOrders/DeleteOrderModal.js
@@ -0,0 +1,68 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import toast from 'react-hot-toast';
+import auth from '../../../firebase.init';
+
+const DeleteOrderModal = ({ deletingOrder, setDeletingOrder, refetch }) => {
+    const { _id, person, email, phone, address, toolName, quantity, totalPrice, status } = deletingOrder;
+    const handleDeleteOrder = id => {
+        fetch(`http://localhost:5000/admin/order/${id}`, {
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            method: 'DELETE'
+        }).then(res => {
+            if (res.status === 401 || res.status === 403) {
+                signOut(auth);
+            }
+            else {
+                return res.json();
+            }
+        }).then(data => {
+            if (data.deletedCount) {
+                toast('Order Deleted',
+                    {
+                        icon: '💨',
+                        style: {
+                            borderRadius: '10px',
+                            background: '#333',
+                            color: '#fff',
+                        },
+                        id: 'orderDeleted'
+                    }
+                );
+            }
+            refetch();
+            setDeletingOrder(null);
+        })
+    }
+    return (
+        <div>
+            <input type="checkbox" id="delete-order-modal" class="modal-toggle" />
+            <div class="modal modal-bottom sm:modal-middle">
+                <div class="modal-box">
+                    <div className='bg-base-300 mt-6 rounded py-2 p-0 md:px-4 md:py-4 -mx-4 md:mx-0'>
+                        <h3 className='font-semibold text-2xl'>Do you Want to Delete the order?</h3>
+                        <hr className='mb-3' />
+                        <p className='text-lg'>Name: <span className='text-primary'>{person}</span></p>
+                        <p className='text-lg'>Email: <span className='text-primary'>{email}</span></p>
+                        <p className='text-lg'>Phone: <span className='text-primary'>{phone}</span></p>
+                        <p className='text-lg'>Address: <span className='text-primary'>{address}</span></p>
+                        <p className='text-lg'>Ordered Tool: <span className='text-primary'>{toolName}</span></p>
+                        <p className='text-lg'>Total Quantity: <span className='text-primary'>{quantity}</span></p>
+                        <p className='text-lg'>Total Price: <span className='text-primary'>{totalPrice}</span></p>
+                        <p className='text-lg'>Status: <span className='text-primary font-semibold text-3xl'>{status}</span></p>
+                    </div>
+                    <div class="modal-action">
+                        <button
+                            onClick={() => handleDeleteOrder(_id)}
+                            className='btn btn-sm btn-error'>Delete</button>
+                        <label for="delete-order-modal" class="btn btn-sm">Cancel</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DeleteOrderModal;
--- src/Pages/Dashboard/ManageOrders/ManageOrders.js
+++ src/Pages/Dashboard/ManageOrders/ManageOrders.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import auth from '../../../firebase.init';
+import LoadingSpinner from '../../Shared/LoadingSpinner';
+import DeleteOrderModal from './DeleteOrderModal';
+import Order from './Order';
 
 const ManageOrders = () => {
+    const [deletingOrder, setDeletingOrder] = useState(null);
+    const { data: orders, isLoading, refetch } = useQuery('orders', () => fetch('http://localhost:5000/admin/order', {
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => {
+        if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+        }
+        else {
+            return res.json();
+        }
+    }));
+
+    if (isLoading) {
+        return <LoadingSpinner />
+    };
     return (
-        <div class="overflow-x-auto">
-            <table class="table w-full">
-                <thead>
-                    <tr>
-                        <td></td>
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className='flex flex-col gap-6'>
+            {
+                orders.map(order => <Order
+                    key={order._id}
+                    refetch={refetch}
+                    order={order}
+                    setDeletingOrder={setDeletingOrder}
+                />)
+            }
+            {
+                deletingOrder && <DeleteOrderModal
+                    deletingOrder={deletingOrder}
+                    setDeletingOrder={setDeletingOrder}
+                    refetch={refetch}
+                />
+            }
         </div>
     );
 };
